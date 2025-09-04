6e00cfdc098fe44314977512bbe24a3937f47289
astor-dev <orangnlp@gmail.com>
2025-09-04 11:10:11 +0900

    feat: 편의를 위해 ddl-auto

--- src/main/java/com/fastcampus/kafkahandson/KafkaHandsOnApplication.java
+++ src/main/java/com/fastcampus/kafkahandson/KafkaHandsOnApplication.java
@@ -2,8 +2,10 @@ package com.fastcampus.kafkahandson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.fastcampus.kafkahandson.data")
 public class KafkaHandsOnApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/fastcampus/kafkahandson/data/MyEntity.java
+++ src/main/java/com/fastcampus/kafkahandson/data/MyEntity.java
@@ -30,7 +30,4 @@ public class MyEntity {
 
     @UpdateTimestamp
     private LocalDateTime updatedAt;
-
-
-
 }
